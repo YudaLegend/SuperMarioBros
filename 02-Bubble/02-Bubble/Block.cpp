488fc26bf148671f143b72ba4ae22bce846624b7
ShinningPikachu <119584676+ShinningPikachu@users.noreply.github.com>
2023-11-09 23:09:38 +0100

    Block

--- 02-Bubble/02-Bubble/Block.cpp
+++ 02-Bubble/02-Bubble/Block.cpp
@@ -4,7 +4,11 @@
 #include <GL/glew.h>
 #include <GL/glut.h>
 
-void Block::init(const glm::ivec2& tileMapPos, ShaderProgram& shaderProgram) {
+void Block::init(const glm::ivec2& tileMapPos, ShaderProgram& shaderProgram, int DayNight) {
+	tileMapDispl = tileMapPos;
+	physics = true;
+	breaking = false;
+	die = false;
 	spritesheet.resize(2);
 	sprite.resize(2);
 	spritesheet[0].loadFromFile("images/Block.png", TEXTURE_PIXEL_FORMAT_RGBA);
@@ -17,10 +21,13 @@ void Block::init(const glm::ivec2& tileMapPos, ShaderProgram& shaderProgram) {
 	sprite[0]->setAnimationSpeed(1, 1);
 	sprite[0]->addKeyframe(0, glm::vec2(0.5f, 0.f));
 
+	sprite[0]->changeAnimation(DayNight);
+	sprite[0]->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
+
 	sprite[1]->Sprite::createSprite(glm::ivec2(64, 96), glm::vec2(1/11.f, 1.f), &spritesheet[1], &shaderProgram);
 	sprite[1]->setNumberAnimations(1);
 
-	sprite[1]->setAnimationSpeed(0, 1);
+	sprite[1]->setAnimationSpeed(0, 11);
 	sprite[1]->addKeyframe(0, glm::vec2(0.f, 0.f));
 	sprite[1]->addKeyframe(0, glm::vec2(1 / 11.f, 0.f));
 	sprite[1]->addKeyframe(0, glm::vec2(2 / 11.f, 0.f));
@@ -33,16 +40,48 @@ void Block::init(const glm::ivec2& tileMapPos, ShaderProgram& shaderProgram) {
 	sprite[1]->addKeyframe(0, glm::vec2(9 / 11.f, 0.f));
 	sprite[1]->addKeyframe(0, glm::vec2(10 / 11.f, 0.f));
 
+	sprite[1]->changeAnimation(0);
+	sprite[1]->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
 
 }
 void Block::update(int deltaTime, float scroll) {
-	for (int i = 0; i < sprite.size(); i++) {
-		sprite[i]->update(deltaTime, scroll);
+	if (breaking) {
+		sprite[1]->update(deltaTime, scroll);
+		breakTimer -= deltaTime;
+		if (breakTimer <= 0) {
+			die = true;
+			physics = false;
+			breaking = false;
+		}
+	}
+	else {
+		sprite[0]->update(deltaTime, scroll);
 	}
 }
 void Block::render() {
-	for (int i = 0; i < sprite.size(); i++) {
-		sprite[i]->render();
+	if (breaking) {
+		sprite[1]->render();
+	}
+	else {
+		sprite[0]->render();
 	}
 }
 
+void Block::breakBlock() {
+	breakTimer = 1000;
+	breaking = true;
+	physics = false;
+}
+
+void Block::setPosition(const glm::vec2& pos) {
+	posPlayer = pos;
+	sprite[0]->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
+}
+
+bool Block::getPhysics() {
+	return physics;
+}
+
+bool Block::getDie() {
+	return die;
+}
--- 02-Bubble/02-Bubble/Block.h
+++ 02-Bubble/02-Bubble/Block.h
@@ -5,10 +5,20 @@
 class Block
 {
 public:
-	void init(const glm::ivec2& tileMapPos, ShaderProgram& shaderProgram);
+	
+	void init(const glm::ivec2& tileMapPos, ShaderProgram& shaderProgram, int DayNight);
 	void update(int deltaTime, float scroll);
 	void render();
+	void breakBlock();
+	void setPosition(const glm::vec2& pos);
+	bool getPhysics();
+	bool getDie();
 private:
+	glm::ivec2 tileMapDispl, posPlayer;
+	int breakTimer;
+	bool breaking;
+	bool physics;
+	bool die;
 	vector <Texture> spritesheet;
 	vector <Sprite*> sprite;
 };
